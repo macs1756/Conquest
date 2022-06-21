6289f23f3ad7c0879ece3d7adb97aee53158a9e3
macs1756 <kindrativmaksum312@gmail.com>
2022-06-21 21:51:30 +0200

    count

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ btnBurger.addEventListener("click", item =>{
 		btnBurger.classList.toggle("header__burger_close");
 		contentBurger.classList.toggle("burger__open");
 });
+
+
+
+//////////////////////ico follow
+
+
+const followBody = document.querySelector("#follow");
+const followCount = document.querySelector(".list__nav-follow");
+let count = 1;
+followBody.addEventListener("click", follow=>{
+	
+	let countNext = ++count;
+	followCount.innerHTML = countNext ;
+});
